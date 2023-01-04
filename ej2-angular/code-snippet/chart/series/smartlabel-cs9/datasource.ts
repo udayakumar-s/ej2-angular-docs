23c97293087f249e6f5acd16faf1828b8bfa9ba9
Build Automaion <buildautomation@syncfusion.com>
2023-01-04 20:08:22 +0530

    Integrated latest changes at 01-04-2023 8:00:06 PM

--- ej2-angular/code-snippet/chart/series/smartlabel-cs9/datasource.ts
+++
@@ -1,6 +0,0 @@
-export let labelData: Object[]= [
-                { x: 'Jan', y: 3, text: 'Jan: 3' }, { x: 'Feb', y: 3.5, text: 'Feb: 3.5' },
-                { x: 'Mar', y: 7, text: 'Mar: 7' }, { x: 'Apr', y: 13.5, text: 'Apr: 13.5' },
-                { x: 'May', y: 19, text: 'May: 19' }, { x: 'Jun', y: 23.5, text: 'Jun: 23.5' },
-                { x: 'Jul', y: 26, text: 'Jul: 26' }, { x: 'Aug', y: 25, text: 'Aug: 25' },
-                { x: 'Sep', y: 21, text: 'Sep: 21' }, { x: 'Oct', y: 15, text: 'Oct: 15' }];
