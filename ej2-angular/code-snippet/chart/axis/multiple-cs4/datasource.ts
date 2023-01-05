211bb4d87a608b2b10dba851a82c9a3e0d86cee5
Build Automaion <buildautomation@syncfusion.com>
2023-01-05 11:07:52 +0530

    Integrated latest changes at 01-05-2023 11:00:05 AM

--- ej2-angular/code-snippet/chart/axis/multiple-cs4/datasource.ts
+++
@@ -1,6 +0,0 @@
-export let labelData: Object[] = [
-                { x: "South Korea", y: 39.4 }, { x: "India", y: 61.3 }, { x: "Pakistan", y: 20.4 },
-                { x: "Germany", y: 65.1 }, { x: "Australia", y: 15.8 }, { x: "Italy", y: 29.2 },
-                { x: "France", y: 44.6 }, { x: "Saudi Arabia", y: 9.7 }, { x: "Russia", y: 40.8 },
-                { x: "Mexico", y: 31 }, { x: "Brazil", y: 75.9 }, { x: "China", y: 51.4 }
-        ];
