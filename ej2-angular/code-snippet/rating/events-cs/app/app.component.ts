f37109ac82e2daef5f032fdc586eadd28dcd7f0f
Build Automaion <buildautomation@syncfusion.com>
2023-01-27 13:15:17 +0530

    Integrated latest changes at 01-27-2023 1:00:09 PM

---
+++ ej2-angular/code-snippet/rating/events-cs/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'app-root',
+  template: `<!-- To Render Rating component. -->
+                <div class="wrap">
+                  <input ejs-rating id="rating" value="3" visible="true" />
+                </div>`
+})
+
+export class AppComponent {
+  
+}
---
+++ ej2-angular/code-snippet/rating/selection/selection-cs/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'app-root',
+  template: `<!-- To Render Rating component. -->
+                <div class="wrap">
+                  <input ejs-rating id="rating" value="3" />
+                </div>`
+})
+
+export class AppComponent { }
