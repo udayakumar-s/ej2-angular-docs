3a9aacb600d6c205172eb87e2dc35fd7b1bc7dd6
Build Automaion <buildautomation@syncfusion.com>
2023-02-06 17:00:10 +0530

    Integrated latest changes at 02-06-2023 2:00:10 PM

---
+++ ej2-angular/code-snippet/common/icons/app/app.component.ts
@@ -0,0 +1,21 @@
+
+
+
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="icon-bar">
+      <span class="e-icons e-cut"></span>
+      <span class="e-icons e-copy"></span>
+      <span class="e-icons e-paste"></span>
+    </div>
+  `
+})
+
+export class AppComponent {
+  
+}
+
+
