68bb71212735259a59058a6f1f22feff6b0ab01b
Build Automaion <buildautomation@syncfusion.com>
2023-03-02 23:19:36 +0530

    Integrated latest changes at 03-02-2023 8:11:33 PM

--- ej2-angular/code-snippet/autocomplete/header-template-cs1/app/app.component.ts
+++ ej2-angular/code-snippet/autocomplete/header-template-cs1/app/app.component.ts
@@ -7,7 +7,7 @@ import { Query, DataManager, ODataV4Adaptor } from '@syncfusion/ej2-data';
 @Component({
     selector: 'app-root',
     // specifies the template url path
-    templateUrl: './headerTemplate.html'
+    templateUrl: './app/headerTemplate.html'
 })
 export class AppComponent {
     constructor() {
