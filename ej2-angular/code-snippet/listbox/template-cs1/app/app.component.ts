28c6bc65ec671ca1b4fe7ec70f8903e184bd778e
Build Automaion <buildautomation@syncfusion.com>
2023-03-16 02:09:08 +0530

    Integrated latest changes at 03-15-2023 8:17:18 PM

--- ej2-angular/code-snippet/listbox/template-cs1/app/app.component.ts
+++ ej2-angular/code-snippet/listbox/template-cs1/app/app.component.ts
@@ -1,23 +1,26 @@
 
 
 
-import { Component  } from '@angular/core';
-
+import { Component, ChangeDetectorRef } from '@angular/core';
 @Component({
     selector: 'app-container',
     templateUrl: 'template.html',
     styleUrls: ['template.css']
 })
 
-export class AppComponent{
-public data: { [key: string]: Object }[] = [
-    { text: 'JavaScript', pic: 'javascript', description: 'It is a lightweight interpreted or JIT-compiled programming language.' },
-    { text: 'TypeScript', pic: 'typeScript', description: 'It is a typed superset of Javascript that compiles to plain JavaScript.' },
-    { text: 'Angular', pic: 'angular', description: 'It is a TypeScript-based open-source web application framework.' },
-    { text: 'React', pic: 'react', description: 'A JavaScript library for building user interfaces. It can also render on the server using Node.' },
-    { text: 'Vue', pic: 'vue', description: 'A progressive framework for building user interfaces. it is incrementally adoptable.' }
-];
+export class AppComponent  {
+    public data: { [key: string]: Object }[];
+    constructor(private cdr: ChangeDetectorRef) {}
+    ngOnInit():void{
+            this.cdr.detectChanges();
+            this.data = [
+                { text: 'JavaScript', pic: 'javascript', description: 'It is a lightweight interpreted or JIT-compiled programming language.' },
+                { text: 'TypeScript', pic: 'typeScript', description: 'It is a typed superset of Javascript that compiles to plain JavaScript.' },
+                { text: 'Angular', pic: 'angular', description: 'It is a TypeScript-based open-source web application framework.' },
+                { text: 'React', pic: 'react', description: 'A JavaScript library for building user interfaces. It can also render on the server using Node.' },
+                { text: 'Vue', pic: 'vue', description: 'A progressive framework for building user interfaces. it is incrementally adoptable.' }
+            ];
+        
+    }
+    
 }
-
-
-
