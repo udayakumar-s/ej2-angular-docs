506520ba93f109138bcaaf03ccf9ac6b540ddb3f
Build Automaion <buildautomation@syncfusion.com>
2023-05-15 18:19:59 +0530

    Integrated latest changes at 05-15-2023 5:00:12 PM

--- ej2-angular/code-snippet/file-manager/access-control-cs1/app/app.component.ts
+++ ej2-angular/code-snippet/file-manager/access-control-cs1/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     styleUrls: ['app/app.component.css'],
-    template: `<ejs-filemanager id='file-manager' [ajaxSettings]='ajaxSettings' [view]='view' (beforeSend)="beforeSend($event)">
+    template: `<ejs-filemanager id='file-manager' [ajaxSettings]='ajaxSettings' [view]='view'>
     </ejs-filemanager>`
 })
 export class AppComponent {
@@ -15,18 +15,14 @@ export class AppComponent {
     public hostUrl: string = 'https://ej2-aspcore-service.azurewebsites.net/';
     public ngOnInit(): void {
         this.ajaxSettings = {
-            url: this.hostUrl + 'api/FileManager/FileOperations',
-            getImageUrl: this.hostUrl + 'api/FileManager/GetImage',
-            uploadUrl: this.hostUrl + 'api/FileManager/Upload',
-            downloadUrl: this.hostUrl + 'api/FileManager/Download'
+            url: this.hostUrl + 'api/FileManagerAccess/FileOperations',
+            getImageUrl: this.hostUrl + 'api/FileManagerAccess/GetImage',
+            uploadUrl: this.hostUrl + 'api/FileManagerAccess/Upload',
+            downloadUrl: this.hostUrl + 'api/FileManagerAccess/Download'
         };
         this.view = "Details";
-       }
-       beforeSend(args){
-        args.ajaxSettings.beforeSend = function (args: any) {
-            args.httpRequest.setRequestHeader('Authorization', 'Access control');
-          };
-       }
+    }
+
 }
 
 
