3e51727d3e7c5560f91387ff9b31800ce072a4d4
Build Automaion <buildautomation@syncfusion.com>
2023-05-17 18:11:09 +0530

    Integrated latest changes at 05-17-2023 4:03:10 PM

---
+++ ej2-angular/code-snippet/grid/row-style-selected-cs1/app/app.component.ts
@@ -0,0 +1,36 @@
+
+
+import { Component, OnInit } from '@angular/core';
+import { data } from './datasource';
+
+@Component({
+    selector: 'app-root',
+    template: `<ejs-grid [dataSource]='data'>
+                <e-columns>
+                    <e-column field='OrderID' headerText='Order ID' textAlign='Right'
+                    width=120></e-column>
+                    <e-column field='CustomerID' headerText='Customer ID' width=140>
+                    </e-column>
+                    <e-column field='Freight' headerText='Freight' textAlign='Right'
+                    format='C' width=120></e-column>
+                    <e-column field='OrderDate' headerText='Order Date' textAlign='Right' 
+                    format='yMd' width=140></e-column>
+                </e-columns>
+               </ejs-grid>`,
+    styles: [`
+        .e-grid .e-selectionbackground {
+            background-color: #f9920b;
+        }
+    `]
+})
+export class AppComponent implements OnInit {
+    
+    public data: object[];
+
+    ngOnInit(): void {
+        this.data = data;
+    }
+}
+
+
+
---
+++ ej2-angular/code-snippet/grid/rows-event-cs1/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GridComponent } from '@syncfusion/ej2-angular-grids';
+import { data } from './datasource';
+
+@Component({
+  selector: 'app-root',
+  template: `<ejs-grid #grid [dataSource]="data" (dataBound)="customizeRows($event)">
+                <e-columns>
+                    <e-column field="OrderID" headerText="Order ID" textAlign="Right"
+                    width="100"></e-column>
+                    <e-column field="CustomerID" headerText="Customer ID" width="120">
+                    </e-column>
+                    <e-column field="Freight" headerText="Freight" textAlign="Right" 
+                    format="C" width="100">
+                    </e-column>
+                    <e-column field="OrderDate" headerText="Order Date" textAlign="Right" 
+                    format="yMd" width="100"></e-column>
+                </e-columns>
+            </ejs-grid>`,
+})
+export class AppComponent implements OnInit {
+
+  public data: object[];
+  @ViewChild('grid') public grid: GridComponent;
+
+  ngOnInit(): void {
+    this.data = data;
+  }
+  public customizeRows(args) {
+    (this.grid.getRowByIndex(2) as any).style.background = 'rgb(193, 228, 234)';
+  }
+}
