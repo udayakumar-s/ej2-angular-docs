8b9ce1d0bf46bcb99b8af9982c3fd1088d0be3b8
Build Automaion <buildautomation@syncfusion.com>
2023-06-23 06:09:59 +0530

    Integrated latest changes at 06-23-2023 1:50:01 AM

---
+++ ej2-angular/code-snippet/grid/columnchooser-cs5/app/app.component.ts
@@ -0,0 +1,32 @@
+
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { data } from './datasource';
+import { ToolbarItems, GridComponent, ColumnChooserSettingsModel } from '@syncfusion/ej2-angular-grids';
+
+@Component({
+    selector: 'app-root',
+    template: `<ejs-grid [dataSource]='data' [toolbar]='toolbarOptions' height='272px' [showColumnChooser]= 'true' [columnChooserSettings]='columnChooserSettings'>
+               <e-columns>
+                    <e-column field='OrderID' headerText='Order ID' width='120' textAlign="Right"></e-column>
+                    <e-column field='OrderDate' headerText='Order Date' width='120' format="yMd" textAlign="Right"></e-column>
+                    <e-column field='Freight' headerText='Freight' width='120' format='C2' textAlign="Right"></e-column>
+                    <e-column field='ShipCountry' headerText='Ship Country' width='130'></e-column>
+                    <e-column field='ShipCity' headerText='Ship City' [visible]='false' width='130'></e-column>
+               </e-columns>
+                </ejs-grid>`
+})
+export class AppComponent implements OnInit {
+
+    @ViewChild('grid') public grid?: GridComponent;
+
+    public data?: object[];
+    public toolbarOptions?: ToolbarItems[];
+    public columnChooserSettings?:ColumnChooserSettingsModel;
+
+    ngOnInit(): void {
+        this.data = data;
+        this.toolbarOptions = ['ColumnChooser'];
+        this.columnChooserSettings = { operator: 'contains' };
+    }
+}
